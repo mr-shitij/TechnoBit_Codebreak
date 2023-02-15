357542ad7dd1c2a055b3917a4c8ec53186a515a7
mr-shitij <21.shitijagrawal@gmail.com>
2023-02-15 21:05:18 +0530

    Signup and Login

---
+++ src/components/AdminLogin.js
@@ -0,0 +1,115 @@
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+import { useRef, useState } from 'react'
+import { useHistory } from 'react-router-dom'
+
+function Copyright(props) {
+    return (
+      <Typography variant="body2" color="text.secondary" align="center" {...props}>
+        {'Copyright © '}
+        <Link color="inherit" href="https://foss.coep.org.in/cofsug">
+          CoFSUG
+        </Link>
+        {'.'}
+      </Typography>
+    );
+}
+  
+const theme = createTheme();  
+
+export function AdminLogin() {
+    const emailRef = useRef()
+    const passwordRef = useRef()
+
+    // Get signUp function from the auth context
+    // const { signIn } = useAuth()
+
+    const history = useHistory()
+
+    async function handleSubmit(event) {
+        event.preventDefault()
+        // Get email and password input values
+        const data = new FormData(event.currentTarget);
+
+        // Calls `signIn` function from the context
+        // const { error } = await signIn({email: data.get('email'), password: data.get('password')})
+
+        // if (error) {
+        //     alert('error signing in')
+        // } else {
+        //     // Redirect user to Dashboard
+        //     history.push('/admin')
+        // }
+    }
+
+    return (
+        <ThemeProvider theme={theme}>
+          <Container component="main" maxWidth="xs">
+            <CssBaseline />
+            <Box
+              sx={{
+                marginTop: 8,
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+              }}
+            >
+              <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+                <LockOutlinedIcon />
+              </Avatar>
+              <Typography component="h1" variant="h5">
+                Admin Sign in
+              </Typography>
+              <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+                <TextField
+                  margin="normal"
+                  required
+                  fullWidth
+                  id="email"
+                  label="Email Address"
+                  name="email"
+                  autoComplete="email"
+                  autoFocus
+                />
+                <TextField
+                  margin="normal"
+                  required
+                  fullWidth
+                  name="password"
+                  label="Password"
+                  type="password"
+                  id="password"
+                  autoComplete="current-password"
+                />
+                <FormControlLabel
+                  control={<Checkbox value="remember" color="primary" />}
+                  label="Remember me"
+                />
+                <Button
+                  type="submit"
+                  fullWidth
+                  variant="contained"
+                  sx={{ mt: 3, mb: 2 }}
+                >
+                  Sign In
+                </Button>
+              </Box>
+            </Box>
+            <Copyright sx={{ mt: 8, mb: 4 }} />
+          </Container>
+        </ThemeProvider>
+    )
+}
